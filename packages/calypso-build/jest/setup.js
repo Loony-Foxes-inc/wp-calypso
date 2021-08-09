6805787a25d3459cf2c0a19b5a51e894f97feeda
Noah Allen <noahtallen@gmail.com>
2021-08-09 15:14:29 -0700

    Fix code lint issue

--- packages/calypso-build/jest/setup.js
+++ packages/calypso-build/jest/setup.js
@@ -1,5 +1,5 @@
-const { configure } = require( 'enzyme' );
 const Adapter = require( '@wojtekmaj/enzyme-adapter-react-17' );
+const { configure } = require( 'enzyme' );
 
 configure( { adapter: new Adapter() } );
 
