1ef97b7b75d1a35977c10fb209fd9000eb56d210
annemirasol <annemirasol@gmail.com>
2021-08-31 17:39:07 +0800

    Disable masterbar if P2 login

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -134,7 +134,10 @@ export default compose(
 		const isJetpackLogin = startsWith( currentRoute, '/log-in/jetpack' );
 		const isWhiteLogin = startsWith( currentRoute, '/log-in/new' );
 		const isJetpackWooDnaFlow = wooDnaConfig( getInitialQueryArguments( state ) ).isWooDnaFlow();
-		const noMasterbarForRoute = isJetpackLogin || isWhiteLogin || isJetpackWooDnaFlow;
+		const isP2Login =
+			startsWith( currentRoute, '/log-in' ) &&
+			'p2' === get( getCurrentQueryArguments( state ), 'from' );
+		const noMasterbarForRoute = isJetpackLogin || isWhiteLogin || isJetpackWooDnaFlow || isP2Login;
 		const isPopup = '1' === get( getCurrentQueryArguments( state ), 'is_popup' );
 		const noMasterbarForSection = [ 'signup', 'jetpack-connect' ].includes( sectionName );
 		const isJetpackWooCommerceFlow =
